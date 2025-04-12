bc856ead1b431fcac04c252c3e438ab792ca14b5
Paridnya Mane <mane.paridnya39@gmail.com>
2025-04-13 04:13:13 +0530

    Add temporary migration view

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import JobPostListCreateView, run_scrapers_view
+from .views import JobPostListCreateView, run_scrapers_view, run_migrations_view
 
 urlpatterns = [
     # List all job posts or allow creating one manually GET
@@ -8,5 +8,6 @@ urlpatterns = [
 
     # Run all scrapers and save jobs to the DB via POST
     path("run-scrapers/", run_scrapers_view, name="run-scrapers"),
+    path('run-migrations/', run_migrations_view),
 
 ]
--- startup_scraper/settings.py
+++ startup_scraper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8o32!7#652@kp(l#4(pp=!r5d-*wflmp_ksc$(y9q^jh@bw%9@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
